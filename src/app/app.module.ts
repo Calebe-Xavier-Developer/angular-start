4a1d78e22bf224c582a8dea750418eb57ad86af9
Calebe-Xavier-Developer <calebexavier.developer@gmail.com>
2021-03-05 14:51:09 -0300

    Start with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course.component';
 import { StarComponent } from './stars/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './404/error-404.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: '**',
+        component: Error404Component,
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
