6d553b7ffdd317e0e1e6478c5a9d427f97c7cec2
Calebe-Xavier-Developer <calebexavier.developer@gmail.com>
2021-03-05 10:57:11 -0300

    Dependence Injectable

--- src/app/courses/course.component.ts
+++ src/app/courses/course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -9,28 +10,9 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) {}
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 99.99,
-        code: 'AGF-2021',
-        duration: 180,
-        rating: 4.8,
-        releaseDate: 'Dezembro, 1, 2020',
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTPClient',
-        imageUrl: '/assets/images/http.png',
-        price: 39.99,
-        code: 'AGH-2021',
-        duration: 30,
-        rating: 4.3,
-        releaseDate: 'Janeiro, 10, 2021',
-      },
-    ];
+    this.courses = this.courseService.retriveAll();
   }
 }
