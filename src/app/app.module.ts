bdaf00e777ac0452e3affcdaaeac87268bfaa3bc
Calebe-Xavier-Developer <calebexavier.developer@gmail.com>
2021-03-05 13:38:34 -0300

    Transform dates and other elements with Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course.component';
 import { StarComponent } from './stars/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent],
+  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
