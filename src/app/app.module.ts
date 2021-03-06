960502ca72a3f177275801c3cc1a05780b0327c6
Calebe-Xavier-Developer <calebexavier.developer@gmail.com>
2021-03-06 01:46:37 -0300

    Segregating the application in modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course.component';
-import { StarComponent } from './stars/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/couser.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Error404Component,
-    CourseInfoComponent,
-  ],
+  declarations: [AppComponent, NavBarComponent, Error404Component],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses',
-        component: CourseListComponent,
-      },
-      {
-        path: 'courses/info/:id',
-        component: CourseInfoComponent,
-      },
       {
         path: '',
         redirectTo: 'courses',
