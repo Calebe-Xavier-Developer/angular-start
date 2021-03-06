d5f4b65db23a1dffb43aa0468a977a026ab35bae
Calebe-Xavier-Developer <calebexavier.developer@gmail.com>
2021-03-06 00:47:56 -0300

    Http resquest added

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -1,24 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Course } from './course';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
-  retriveAll(): Course[] {
-    return COURSES;
+  private coursesUrl: string = 'http://localhost:3100/api/courses';
+
+  constructor(private httpClient: HttpClient) {}
+
+  retriveAll(): Observable<Course[]> {
+    return this.httpClient.get<Course[]>(this.coursesUrl);
   }
 
-  retrieveById(id: number): Course {
-    return COURSES.find((courseItereator: Course) => courseItereator.id === id);
+  retrieveById(id: number): Observable<Course> {
+    return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`);
   }
 
-  save(course: Course): void {
+  save(course: Course): Observable<Course> {
     if (course.id) {
-      const index = COURSES.findIndex(
-        (courseItereator: Course) => courseItereator.id === course.id
+      return this.httpClient.put<Course>(
+        `${this.coursesUrl}/${course.id}`,
+        course
       );
-      COURSES[index] = course;
+    } else {
+      return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
     }
   }
 }
